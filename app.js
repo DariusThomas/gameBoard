acd6204010b224c176001a73483640e6570f1f71
Darius <dariusd.thomas@gmail.com>
2019-03-13 14:32:48 -0400

    declare final results

--- app.js
+++ app.js
@@ -16,19 +16,24 @@ cells.forEach(function(cell){
 });
 
 function cellClicked(e){
-    if(count == 9){
-        count = 0
-        resetBoard(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9)
+    if(count == 9 && results.textContent == "Tic Tac Toe"){
+        count = -1;
+        results.textContent = "Draw";
     } else if(count % 2 == 0 && e.target.textContent == ""){
         count ++  
         e.target.textContent ="X"
     }else if(count % 2 == 1 && e.target.textContent == ""){   
 count ++
     e.target.textContent ="O"  
-}else{
+}else if ( count == -1){
+    count = 0;
+    resetBoard(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9)
+}else {
     count = count + 0
-}
-
+};
+if(e.target.textContent != ""){
+checkWin(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9, e.target.textContent);
+};
 }
  function resetBoard(a, b, c, d, e, f, g, h, i){
      a.textContent = "";
@@ -40,5 +45,19 @@ count ++
      g.textContent = "";
      h.textContent = "";
      i.textContent = "";
+     results.textContent = 'Tic Tac Toe';
  };
 
+ function checkWin(a,b,c,d,e,f,g,h,i,turn){
+     if( a.textContent == turn && b.textContent == turn && c.textContent == turn ||
+        d.textContent == turn && e.textContent == turn && f.textContent == turn ||
+        g.textContent == turn && h.textContent == turn && i.textContent == turn ||
+        a.textContent == turn && d.textContent == turn && g.textContent == turn ||
+        b.textContent == turn && e.textContent == turn && h.textContent == turn ||
+        c.textContent == turn && f.textContent == turn && i.textContent == turn ||
+        a.textContent == turn && e.textContent == turn && i.textContent == turn ||
+        c.textContent == turn && e.textContent == turn && g.textContent == turn){
+            results.textContent = turn + " is the winner";
+            count = -1;
+        }
+ }
